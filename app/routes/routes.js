28a708f88f7eb2975a5e6efcab14443b794889f4
Pamela1984219 <86883731+Pamela1984219@users.noreply.github.com>
2021-08-19 00:04:34 -0400

    routes descomentadas

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 
 app.use(require('../controllers/categorias/categorias.routes'))
-//app.use(require('../controllers/productos/productos.routes'))
-//app.use(require('../controllers/usuarios/usuarios.routes'))
+app.use(require('../controllers/productos/productos.routes'))
+app.use(require('../controllers/usuarios/usuarios.routes'))
 
 
 module.exports = app
