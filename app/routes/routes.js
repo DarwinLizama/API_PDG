210db0484ca9f4143b1dffc23aae656d976fb50e
Darwin Andrés Lizama Ramirez <dlizamaramirez@gmail.com>
2021-08-19 00:21:57 -0400

    Rutas Completas

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -3,6 +3,9 @@ const app = express()
 
 
 app.use(require('../controllers/usuarios/usuarios.routes'))
+app.use(require('../controllers/categorias/categorias.routes'))
+app.use(require('../controllers/productos/productos.routes'))
+
 
 
 module.exports = app
